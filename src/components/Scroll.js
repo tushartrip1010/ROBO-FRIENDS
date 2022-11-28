2a99a8700db395ba18b77d164212229035156db2
Tushar Tripathi <72696630+tushartrip1010@users.noreply.github.com>
2022-11-28 12:25:11 +0530

    robo friends

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -5,7 +5,7 @@ const Scroll = (props) => {
     <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
       {props.children}
     </div>
-  );
+    );
 };
 
 export default Scroll;
